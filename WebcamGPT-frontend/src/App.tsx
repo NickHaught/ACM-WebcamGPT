134807fb9e50852e2c372f47cdd423442b384b7b
Nick Haught <nicholas.haught@outlook.com>
2024-03-25 13:26:13 -0400

    Added webcam functionality

--- WebcamGPT-frontend/src/App.tsx
+++ WebcamGPT-frontend/src/App.tsx
@@ -1,12 +1,13 @@
+import React from 'react'
 import './App.css'
-import Camera from './components/Camera'
 import GPT from './components/GPT'
+import Webcam from './components/CustomWebcam'
 
 function App() {
 
   return (
     <>
-      <Camera/>
+      <Webcam/>
       <GPT/>
     </>
   )
---
+++ WebcamGPT-frontend/src/components/CustomWebcam.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Webcam from "react-webcam"
+
+const CustomWebcam = () => {
+  return (
+    <div className="container">
+    <Webcam height={600} width={600} />
+  </div>
+  )
+}
+
+export default CustomWebcam
